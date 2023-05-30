c6916b09eb28827d0fad4abfc48e0ec58d4a924d
Berk Work <berk.ertan@commencis.com>
2023-05-30 14:39:36 +0300

    improvements

--- src/test/java/steps/EbayHomeSteps.java
+++ src/test/java/steps/EbayHomeSteps.java
@@ -3,30 +3,41 @@ package steps;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import pages.HomePage;
 import tests.BaseTest;
+import org.assertj.core.api.SoftAssertions;
 import utils.DriverManager;
 
 import java.time.Duration;
 
 public class EbayHomeSteps {
 
-    BaseTest baseTest;
+    BaseTest testFunctions = new BaseTest();
     WebDriver driver = Hooks.getDriver();
+    protected SoftAssertions softAssertion = new SoftAssertions();
 
-    @Given("I am on Ebay Home Page")
-    public void i_am_on_ebay_home_page() {
+    // declaring POMs'
+    HomePage homePage;
+
+    @Given("I am on Tech Crunch Home Page")
+    public void home_page() {
         System.out.println("perform the first");
+        homePage = new HomePage(driver);
     }
 
-    @When("I click on Advanced Link")
-    public void i_click_on_advanced_link() {
-        System.out.println("I click on Advanced Link but not with code aq");
-        // WebElement loginButton = homePage.getElement(homePage.loginButtonSelector);
-        //loginButton.click();
+    @When("I click on Login Button")
+    public void click_on_login_button() {
+        WebElement loginButton = homePage.getElement(homePage.loginButtonSelector);
+        loginButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
-    @Then("I navigate to Advanced Search page")
+    @Then("I navigate to Login page")
     public void i_navigate_to_advanced_search_page() {
-        System.out.println("I came I saw praise to Lord");
+        WebElement theLatestNewsElement;
+        softAssertion.assertThat(homePage.getBrowserTitle())
+                .as("Element Text Content Check for login button ")
+                .doesNotContain("TechCrunch");
+        softAssertion.assertAll();
     }
 }
